266c236465e23826594a6838f7c76f4e11950f66
Akash <mrudul@mabventures.com>
2019-11-04 18:24:24 +0530

    delete

--- contactform/src/routes/web.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-Route::group(['namespace' => 'Samuelayo\Contactform\Http\Controllers', 'middleware' => ['web']], function(){
-    Route::get('contact', 'ContactFormController@index');
-    Route::post('contact', 'ContactFormController@sendMail')->name('contact');
-});
